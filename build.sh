e018235209aa98128f4cb75e3154de203aafbfbc
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2019-08-12 23:13:04 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 set -e
-pushd /home/packer/workspace
-packer validate bionic64.json
-time packer build bionic64.json
-packer validate bionic64-nginx.json
-time packer build bionic64-nginx.json
+time packer -var "wd=/home/packer/workspace" validate bionic64.json
+time packer -var "wd=/home/packer/workspace" build bionic64.json
+time packer -var "wd=/home/packer/workspace" validate bionic64-nginx.json
+time packer -var "wd=/home/packer/workspace" build bionic64-nginx.json
 rm -fr  packer_cache/ output-*/
