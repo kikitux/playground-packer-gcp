10702d0af3e03a0cb326c345a4ce346b95088c9d
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2019-08-13 21:02:04 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-set -e
-time packer -var "wd=/home/packer/workspace" validate bionic64.json
-time packer -var "wd=/home/packer/workspace" build bionic64.json
-time packer -var "wd=/home/packer/workspace" validate bionic64-nginx.json
-time packer -var "wd=/home/packer/workspace" build bionic64-nginx.json
-rm -fr  packer_cache/ output-*/
